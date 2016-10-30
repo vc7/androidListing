c4aa7f1d5d81b6bfc734038e409046d4b7b7d7ce
vc7 <vince78718@gmail.com>
2016-10-30 21:44:12 +0800

    Initial Commit.

---
+++ app/src/main/java/com/vc7/listing/MainActivity.java
@@ -0,0 +1,13 @@
+package com.vc7.listing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
