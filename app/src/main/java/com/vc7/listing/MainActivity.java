b3f05bdf8e2915a4c330067ad5aa031dcafba594
vc7 <vince78718@gmail.com>
2016-10-31 02:48:37 +0800

    Add simple list view.

--- app/src/main/java/com/vc7/listing/MainActivity.java
+++ app/src/main/java/com/vc7/listing/MainActivity.java
@@ -2,12 +2,31 @@ package com.vc7.listing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    AdapterView.OnItemClickListener listener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Log.d("ListView", "clicked " + position);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+        ArrayAdapter listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new String[]{"a", "b", "c"});
+
+        listView.setAdapter(listAdapter);
+        listView.setOnItemClickListener(listener);
+
     }
 }
